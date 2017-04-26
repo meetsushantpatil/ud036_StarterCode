41da828f9c70954945134625b13ab9305eb3c3cf
Sushant Patil <meetsushantpatil@gmail.com>
2017-04-25 20:10:13 -0400

    modifying with reviewer's comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,46 @@
-import media
 import fresh_tomatoes
+import media
 
-##Movie objects
-##Add or remove Movie instances to add or remove your favourite movies. These are the ones for your reference.
-##PS - Tom Hanks is my favourite.
+#Movie objects
+#Add or remove Movie instances to add or remove your favourite movies. These are the ones for your reference.
+#PS - Tom Hanks is my favourite.
 
-toy_story = media.Movie("Toy Story",
-                            "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",
-                            "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie(
+            "Toy Story",
+            "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",
+            "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-apollo13 = media.Movie("Apollo 13",
-                            "http://img.moviepostershop.com/apollo-13-movie-poster-1995-1020190947.jpg",
-                            "https://www.youtube.com/watch?v=nEl0NsYn1fU")
+apollo13 = media.Movie(
+	   "Apollo 13",
+           "http://img.moviepostershop.com/apollo-13-movie-poster-1995-1020190947.jpg",
+           "https://www.youtube.com/watch?v=nEl0NsYn1fU")
 
-sleepless_in_seattle = media.Movie("Sleepless In Seattle",
-                            "http://www.gstatic.com/tv/thumb/movieposters/14843/p14843_p_v8_aa.jpg",
-                            "https://www.youtube.com/watch?v=-Lj2U-cmyek")
+sleepless_in_seattle = media.Movie(
+           "Sleepless In Seattle",
+           "http://www.gstatic.com/tv/thumb/movieposters/14843/p14843_p_v8_aa.jpg",
+           "https://www.youtube.com/watch?v=-Lj2U-cmyek")
 
-a_hologram_for_the_king = media.Movie("A Hologram For The King",
-                                  "https://i.jeded.com/i/a-hologram-for-the-king-2016.51316.jpg",
-                                  "https://www.youtube.com/watch?v=UW4OE1egbHs")
+a_hologram_for_the_king = media.Movie(
+           "A Hologram For The King",
+           "https://i.jeded.com/i/a-hologram-for-the-king-2016.51316.jpg",
+           "https://www.youtube.com/watch?v=UW4OE1egbHs")
 
-shawshank_redemption = media.Movie("Shawshank Redemption",
-                                  "http://t0.gstatic.com/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm",
-                                  "https://www.youtube.com/watch?v=RLw6hBFJ8bk")
+shawshank_redemption = media.Movie(
+           "Shawshank Redemption",
+           "http://t0.gstatic.com/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm",
+           "https://www.youtube.com/watch?v=RLw6hBFJ8bk")
 
-captain_philips = media.Movie("Captain Philips",
-                                  "http://www.sonypictures.com/movies/captainphillips/assets/images/onesheet.jpg",
-                                  "https://www.youtube.com/watch?v=a2AiLlYdJy8")
+captain_philips = media.Movie(
+	   "Captain Philips",
+           "http://www.sonypictures.com/movies/captainphillips/assets/images/onesheet.jpg",
+           "https://www.youtube.com/watch?v=a2AiLlYdJy8")
 
-hobbit_an_unexpected_journey = media.Movie("Hobbit - An Unexpected Journey",
-                                  "http://t3.gstatic.com/images?q=tbn:ANd9GcTS1VqOgP7iJC44UcztFaTbvD0OzoRymEhXfMPlgq7FPY0OEvCj",
-                                  "https://www.youtube.com/watch?v=y2M8BbTfZTA")
+hobbit_an_unexpected_journey = media.Movie(
+           "Hobbit - An Unexpected Journey",
+           "http://t3.gstatic.com/images?q=tbn:ANd9GcTS1VqOgP7iJC44UcztFaTbvD0OzoRymEhXfMPlgq7FPY0OEvCj",
+           "https://www.youtube.com/watch?v=y2M8BbTfZTA")
 
-##Do not forget to add the instance name to this list.
+#Do not forget to add the instance name to this list.
 movie_list =  [toy_story,
                apollo13,
                sleepless_in_seattle,
@@ -42,5 +49,5 @@ movie_list =  [toy_story,
                captain_philips,
                hobbit_an_unexpected_journey]
 
-##Creating a static webpage & displaying the movie posters
+#Creating a static webpage & displaying the movie posters
 fresh_tomatoes.open_movies_page(movie_list)
--- media.py
+++ media.py
@@ -1,7 +1,11 @@
-##Class to define movie object
 class Movie():
-    ##Constructor to define the class Movie
+	'''
+	Class to define movie object
+	'''
     def __init__(self, title,poster_image_url, trailer_youtube_url):
+	'''
+	Constructor to define the class Movie
+	'''
         self.title = title;
         self.poster_image_url = poster_image_url;
         self.trailer_youtube_url = trailer_youtube_url;
